7ab7004fb6f4bd2755a5c86bceebae2e0c238938
Vadim <yes.kiselev@gmail.com>
2026-02-08 21:39:16 +0400

    изменяем middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,23 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const host = request.headers.get('host')
-  const url = request.nextUrl.clone()
-
-  // Если запрос пришел на www.ultima-detailing.ru
-  if (host === 'www.ultima-detailing.ru') {
-    url.hostname = 'ultima-detailing.ru'
-    // Делаем 301 редирект на версию без www
-    return NextResponse.redirect(url, 301)
+  const host = request.headers.get('host');
+  
+  // Проверяем наличие www в начале строки
+  if (host && host.startsWith('www.')) {
+    // Очищаем хост от www и возможного порта :10000
+    const newHost = host.replace('www.', '').split(':')[0];
+    const newUrl = new URL(request.nextUrl.pathname + request.nextUrl.search, `https://${newHost}`);
+    
+    return NextResponse.redirect(newUrl, 301);
   }
 }
 
 export const config = {
-  // Применять middleware ко всем путям
-  matcher: '/:path*',
+  matcher: [
+    /*
+     * Исключаем статические файлы и API, чтобы не замедлять их:
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 }
